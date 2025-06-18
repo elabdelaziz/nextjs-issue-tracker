68d828b7ab777dbc49e0cce0c031ba5489fd0ccc
Abdulaziz Omar <elabdulazizomar@gmail.com>
2025-06-18 17:48:27 +0300

    feat: Handle create issue form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,37 @@
-import { Button, TextArea, TextField } from '@radix-ui/themes'
-import React from 'react'
+"use client";
 
-const newIssue = () => {
-  return (
-    <div className="w-full flex justify-center">
-      <div className="max-w-lg space-y-4 w-full mt-12 flex flex-col">
-        <TextField.Root placeholder="Title..." />
-        <TextArea placeholder="description" />
-        <Button>Submit</Button>
-      </div>
-    </div>
-  );
+import { Button, TextArea, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default newIssue
+const newIssue = () => {
+  const { register, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
+
+  const onSubmit = async (data: IssueForm) => {
+    const request = await axios.post("/api/issues", data);
+    router.push("/issues");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full flex justify-center"
+    >
+      <div className="max-w-lg space-y-4 w-full mt-12 flex flex-col">
+        <TextField.Root {...register("title")} placeholder="Title..." />
+        <TextArea {...register("description")} placeholder="description" />
+        <Button>Submit</Button>
+      </div>
+    </form>
+  );
+};
+
+export default newIssue;
