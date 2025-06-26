526c9699fcec150eeac90e12674bb2b44d16cac6
Abdulaziz Omar <elabdulazizomar@gmail.com>
2025-06-26 06:13:18 +0300

    chore: Adding Edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { prisma } from '@/prisma/client'
 import React from 'react'
 import { notFound } from "next/navigation";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import IssueStatus from "@/app/components/IssueStatus";
 
 interface Props {
@@ -18,14 +18,19 @@ const IssueDetailsPage = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <Flex direction="column" gap="4">
-      <Heading as="h2">{issue.title}</Heading>
-      <Flex gap="4">
-        <IssueStatus status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="4">
+        <Heading as="h2">{issue.title}</Heading>
+        <Flex gap="4">
+          <IssueStatus status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card>{issue.description}</Card>
       </Flex>
-      <Card>{issue.description}</Card>
-    </Flex>
+      <Box>
+        <Button>Edit</Button>
+      </Box>
+    </Grid>
   );
 };
 
